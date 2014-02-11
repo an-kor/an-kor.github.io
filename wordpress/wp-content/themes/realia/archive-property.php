8fb0ec2550166ba6f320e0d7b436baed67bae973
root <root@localhost.localdomain>
2014-02-11 18:45:44 +0400

    folder removal

--- wordpress/wp-content/themes/realia/archive-property.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $wp_query;
-
-echo View::render('archive-property.twig', array(
-    'wp_query' => aviators_properties_filter(TRUE),
-    'properties' => aviators_properties_filter(FALSE),
-));
-
