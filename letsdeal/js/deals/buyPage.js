8e3d67d61713d74f43d28196df4a361b1a9cc272
Andrew Korolkov <andrew.korolkov@gmail.com>
2014-06-16 17:56:02 +0400

    disable e-commerce tracking

--- letsdeal/js/deals/buyPage.js
+++ letsdeal/js/deals/buyPage.js
@@ -1,24 +1,27 @@
 Deals.showBuyPage = function(dealId){
     var data = Deals.loadedDeals[dealId];
-    ga("send", "pageview", {
-      'page': "/#/buy/"+dealId,
-      'title': data.title
-    });
+    if (ga) {
+        ga("send", "pageview", {
+          'page': "/#/buy/"+dealId,
+          'title': data.title
+        });
+        /*
+        var trId = data.id + "_" + Math.ceil(Math.random()*100000);
+        ga('ecommerce:addTransaction', {
+          'id': trId,
+          'affiliation': data.title,
+          'revenue': data.price
+        });
 
-    var trId = data.id + "_" + Math.ceil(Math.random()*100000);
-    ga('ecommerce:addTransaction', {
-      'id': trId,
-      'affiliation': data.title,
-      'revenue': data.price
-    });
+        ga('ecommerce:addItem', {
+          'id': trId,
+          'name': data.title,
+          'sku': data.id
+        });
 
-    ga('ecommerce:addItem', {
-      'id': trId,
-      'name': data.title,
-      'sku': data.id
-    });
-
-    ga('ecommerce:send');
+        ga('ecommerce:send');
+        */
+    }
 
     App.showExternalPage(data.title, Messages.buySrc.replace("%DEAL_ID%", data.id));
     App.changeHash('/buy/'+dealId);
